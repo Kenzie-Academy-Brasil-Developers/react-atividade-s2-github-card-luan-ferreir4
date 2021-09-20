c472d42d0779428200e22f13ef3af83c5e6bcb47
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-09-20 15:32:39 -0300

    feat: get the result from InputSearch

--- src/components/Repositories/index.jsx
+++ src/components/Repositories/index.jsx
@@ -1,9 +1,10 @@
 import './style.css'
-// import RepoCard from '../RepoCard'
-export default function Repositories () {
+import RepoCard from '../RepoCard'
+export default function Repositories ({ result: { owner, name, description, url } }) {
+    
     return(
-        <div>
-            
-        </div>
+        <div className='reposContainer'>
+            <RepoCard ownerData={owner}repoName={name} repoDscpt={description}/>
+        </div> 
     );
 }
